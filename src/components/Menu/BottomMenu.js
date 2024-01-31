e625c83ec444e5e111dea0ab03c01025519b0034
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-01-31 17:56:47 +0000

    Wednesday, January 31, 2024 at 6:56 PM West Africa Standard Time

--- src/components/Menu/BottomMenu.js
+++ src/components/Menu/BottomMenu.js
@@ -14,15 +14,16 @@ function BottomMenu() {
           <div className="">
             <img src={Heart} alt="Logo" />
           </div>
-          <div class="border-l-2 opacity-10 border-black h-8 hidden md:block"></div>
-          <div className=" flex items-center  space-x-[4px] ">
-            <img src={Globe} alt="Logo" className="w-[19px] h-[19px]" />
-            <span className="opacity-10 text-black text-lg font-normal ">
-              Public
-            </span>
+          <div className="flex space-x-2.5">
+            <div class="border-l-2 opacity-10 border-black h-8 hidden md:block"></div>
+            <div className=" flex items-center  space-x-[4px] ">
+              <img src={Globe} alt="Logo" className="w-[19px] h-[19px]" />
+              <span className="opacity-10 text-black text-lg font-normal ">
+                Public
+              </span>
+            </div>
+            <div class="hidden md:block border-l-2 opacity-10 border-black h-8"></div>
           </div>
-
-          <div class="hidden md:block border-l-2 opacity-10 border-black h-8"></div>
         </div>
       </div>
     </div>
