226b7c4ca9774594a5944a2b9418d49784536889
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-01-31 12:01:45 +0000

    Wednesday, January 31, 2024 at 1:01 PM West Africa Standard Time

---
+++ src/components/Menu/menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Logo from "../../assests/logo/Logo.png";
+function NavBar() {
+  return (
+    <div>
+      <div>
+        <img src={Logo} alt="Logo" />
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
