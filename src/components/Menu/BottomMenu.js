a318eb5cb84f05132b0ac3264af77dd5a2749da4
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-01-31 18:13:41 +0000

    Wednesday, January 31, 2024 at 7:13 PM West Africa Standard Time

--- src/components/Menu/BottomMenu.js
+++ src/components/Menu/BottomMenu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Heart from "../../assests/icons/Heart.png";
 import Globe from "../../assests/icons/globe-2-outline.png";
+import Hard from "../../assests/icons/Hard.png";
 
 function BottomMenu() {
   return (
@@ -10,19 +11,21 @@ function BottomMenu() {
           Brackets
         </h1>
 
-        <div className="flex items-center space-x-[20px] ">
+        <div className="flex items-center space-x-[9px] ">
           <div className="">
             <img src={Heart} alt="Logo" />
           </div>
           <div className="flex space-x-2.5">
-            <div class="border-l-2 opacity-10 border-black h-8 hidden md:block"></div>
+            <div class="border-l-[1px] opacity-10 border-black h-8 hidden md:block"></div>
             <div className=" flex items-center  space-x-[4px] ">
               <img src={Globe} alt="Logo" className="w-[19px] h-[19px]" />
               <span className="opacity-10 text-black text-lg font-normal ">
                 Public
               </span>
             </div>
-            <div class="hidden md:block border-l-2 opacity-10 border-black h-8"></div>
+          </div>
+          <div className="">
+            <img src={Hard} alt="Hard Drive" />
           </div>
         </div>
       </div>
