a62150cbf84f28a5370aee7571dbeba40aa9f882
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-02-01 12:41:44 +0000

    Thursday, February 1, 2024 at 1:41 PM West Africa Standard Time

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Ellipse from "../assests/icons/Combined-Shape.png"
 
-function div() {
+function Header() {
   return (
     <div>
-      <div>
+      <div className="flex justify-between">
         <p>Design</p>
-        
+        <img  src={Ellipse} alt="Ellipse" />
+
       </div>
     </div>
   );
